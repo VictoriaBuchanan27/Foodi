b8d369de5b9027657c9f4a4c2344d3796bf41610
Victoria Buchanan <victoriabuchanan@pursuit.org>
2019-03-14 23:21:13 -0400

    login page completed

---
+++ html/Logo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class LoadingPage extends Component{
+    constructor(props){
+        super (props);
+        this.state={
+            active= false,
+            time= 0,
+        }
+    }
+    onClick=(e)=>{
+        if (this.state.active===true){
+            
+        }
+    }
+}
